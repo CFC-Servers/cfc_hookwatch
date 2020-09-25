3391860712e90cf3fecfede228e262d3354762b6
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-09-24 22:09:29 -0400

    Hotfix

--- lua/autorun/cfc_hookwatch.lua
+++ lua/autorun/cfc_hookwatch.lua
@@ -9,7 +9,7 @@ local function wrapHookAdd()
     hook.Add = function( eventName, identifier, func )
         if not isstring( identifier ) then
             Alerter:warn( "A hook with a non-string identifier was added! This can cause performance issues if the hook is called frequently.", eventName, identifier )
-            trace.traceback()
+            debug.traceback()
         end
 
         originalHookAdd( eventName, identifier, func )
