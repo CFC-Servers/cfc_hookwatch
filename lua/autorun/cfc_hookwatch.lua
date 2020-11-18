14d749990a6952371aad0034b2d9ae48630d05e2
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-11-17 17:19:19 -0800

    Set log line to debug

--- lua/autorun/cfc_hookwatch.lua
+++ lua/autorun/cfc_hookwatch.lua
@@ -1,14 +1,14 @@
 AddCSLuaFile()
 
 require( "cfclogger" )
-local Alerter = CFCLogger( "HookWatch", "debug" )
+local Alerter = CFCLogger( "HookWatch" )
 
 local function wrapHookAdd()
     local originalHookAdd = hook.Add
 
     hook.Add = function( eventName, identifier, func )
         if not isstring( identifier ) then
-            Alerter:warn( "A hook with a non-string identifier was added! This can cause performance issues if the hook is called frequently.", eventName, identifier )
+            Alerter:debug( "A hook with a non-string identifier was added! This can cause performance issues if the hook is called frequently.", eventName, identifier )
             debug.traceback()
         end
 
